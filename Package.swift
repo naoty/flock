098f6badadb2627853b0c1b7142a171bb3ff3d26
Naoto Kaneko <naoty.k@gmail.com>
2017-02-28 14:25:39 +0900

    Add jpsim/SourceKitten as a dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "flock"
+    name: "flock",
+    dependencies: [
+        .Package(url: "https://github.com/jpsim/SourceKitten", majorVersion: 0)
+    ]
 )
